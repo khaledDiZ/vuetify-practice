adb24d0a53f3dee3ed5c1c4fbe9b0f1e252b227f
Khaled <disasterz9955@gmail.com>
2019-05-12 01:26:32 +0300

    toolbar & navigation drawer setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninga</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
+
