e373e3126b6599c20408ed7cd76fd49249ee3042
Khaled <disasterz9955@gmail.com>
2019-05-12 02:09:16 +0300

    sidebar is done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,16 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
-      <p>test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -23,7 +32,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
     };
   }
 };
