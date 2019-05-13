309b509bcc39b86388fdab72fa06e9c52fc853e2
Khaled <disasterz9955@gmail.com>
2019-05-13 22:47:39 +0300

    Add datepicke and date-formatter

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,18 +1,29 @@
 <template>
   <v-dialog max-width="600px">
-    <v-btn flat slot="activator" class="success">
-      <v-icon left>add</v-icon>
-      <span>Add new project</span>
-    </v-btn>
+    <v-btn flat slot="activator" class="success">Add New Project</v-btn>
     <v-card>
       <v-card-title>
-        <h2>Add new project</h2>
+        <h2>Add a New Project</h2>
       </v-card-title>
       <v-card-text>
         <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
-          <v-btn flat class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+          <v-text-field v-model="title" label="Title" prepend-icon="folder"></v-text-field>
+          <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+
+          <v-menu v-model="menu" :close-on-content-click="false">
+            <v-text-field
+              slot="activator"
+              :value="formattedDate"
+              clearable
+              label="Due date"
+              prepend-icon="date_range"
+            ></v-text-field>
+            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+
+          <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -20,18 +31,26 @@
 </template>
 
 <script>
+import format from "date-fns/format";
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      due: null,
+      menu: false
     };
   },
   methods: {
     submit() {
       console.log(this.title, this.content);
     }
+  },
+  computed: {
+    formattedDate() {
+      console.log(this.due);
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
+    }
   }
 };
 </script>
-
